2c777beb4b2251f326182bf93b2373680827fce4
XC3000 <richardrozario.rr@gmail.com>
2020-09-02 23:55:12 +0530

    card adjusted height

--- src/components/coupons/HomeCoupons.js
+++ src/components/coupons/HomeCoupons.js
@@ -5,10 +5,7 @@ function HomeCoupons() {
     <div className="row">
       <div className="col-12 col-md-4 col-lg-4 mb-2">
         <div className="card h-100">
-          <div className="ribbon ribbon-top-left">
-            <span>Best Seller</span>
-          </div>
-          {/* <div class="ribbon ribbon-top-left">
+          {/* <div className="ribbon ribbon-top-left">
             <span>Best Seller</span>
           </div> */}
           <a href>
@@ -73,30 +70,22 @@ function HomeCoupons() {
           </div>
         </div>
       </div>
-      <div className="col-12 col-md-4 col-lg-4">
-        {/* <div class="minus">
-          <span>-100%</span>
-        </div> */}
+
+      <div className="col-12 col-md-4 col-lg-4 mb-2">
         <div className="card h-100">
-          {/* <div class="ribbon ribbon-top-left">
+          {/* <div className="ribbon ribbon-top-left">
             <span>Best Seller</span>
           </div> */}
-          <div className="ribbon ribbon-top-left">
-            <span style={{ backgroundColor: "#ff4133" }}>Top rated</span>
-          </div>
-          <div className="minus">
-            <span>-100%</span>
-          </div>
           <a href>
             <img
               width={240}
               height={135}
-              className="card-img-top img-fluid p-3"
+              className="card-img-top img-fluid p-2"
               src="https://tutorialforall.com/wp-content/uploads/thumbs_dir/after-effects-motion-graphics-for-beginners-1wyhcd99yjfxynxhwzzoy6ixog7v4k623rk21kvqbt58.jpg"
               alt="Card image cap"
             />
           </a>
-          <div className="card-body" style={{ padding: "1rem" }}>
+          <div className="card-body" style={{ padding: "0.5rem" }}>
             <div className="row py-2">
               <div className="col-9">
                 <span
@@ -149,156 +138,9 @@ function HomeCoupons() {
           </div>
         </div>
       </div>
-      <div className="col-12 col-md-4 col-lg-4">
-        <div className="card h-100">
-          <img
-            width={240}
-            height={135}
-            className="card-img-top p-2"
-            src="https://via.placeholder.com/150"
-            alt="Card image cap"
-          />
-          <div className="card-body" style={{ padding: "1rem" }}>
-            <div className="row py-2">
-              <div className="col-9">
-                <span
-                  style={{ textDecoration: "line-through", paddingRight: 5 }}
-                >
-                  $ 0
-                </span>
-                <span style={{ color: "#2a89ee" }}>Free</span>
-              </div>
-              <div className="col-3">
-                <img
-                  src={require("../../assets/img/google-logo.png")}
-                  alt
-                  sizes
-                  srcSet
-                  style={{ width: 25, height: 25 }}
-                />
-              </div>
-            </div>
-            <h5 className="card-title" style={{ fontSize: 15 }}>
-              Photoshop Mega Course – From Beginner to Super Designer
-            </h5>
-            <div className="row">
-              <div className="col-12 col-lg-6" style={{ fontSize: 12 }}>
-                <p>
-                  <span style={{ textTransform: "uppercase", fontSize: 10 }}>
-                    PHOTOSHoP
-                  </span>
-                  <br />
-                  <span style={{ fontSize: 10 }}>Udemy</span>
-                </p>
-              </div>
-              <div className="col-12 col-lg-6" style={{ fontSize: 12 }}>
-                <p className="text-muted">
-                  <i className="far fa-clock" /> 1 days left
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-12 col-md-4 col-lg-4">
-        <div className="card h-100">
-          <img
-            className="card-img-top p-2"
-            src="https://via.placeholder.com/150"
-            alt="Card image cap"
-          />
-          <div className="card-body" style={{ padding: "1rem" }}>
-            <div className="row py-2">
-              <div className="col-9">
-                <span
-                  style={{ textDecoration: "line-through", paddingRight: 5 }}
-                >
-                  $ 0
-                </span>
-                <span style={{ color: "#2a89ee" }}>Free</span>
-              </div>
-              <div className="col-3">
-                <img
-                  src={require("../../assets/img/google-logo.png")}
-                  alt
-                  sizes
-                  srcSet
-                  style={{ width: 25, height: 25 }}
-                />
-              </div>
-            </div>
-            <h5 className="card-title" style={{ fontSize: 15 }}>
-              Photoshop Mega Course – From Beginner to Super Designer
-            </h5>
-            <div className="row">
-              <div className="col-12 col-lg-6" style={{ fontSize: 12 }}>
-                <p>
-                  <span style={{ textTransform: "uppercase", fontSize: 10 }}>
-                    PHOTOSHoP
-                  </span>
-                  <br />
-                  <span style={{ fontSize: 10 }}>Udemy</span>
-                </p>
-              </div>
-              <div className="col-12 col-lg-6" style={{ fontSize: 12 }}>
-                <p className="text-muted">
-                  <i className="far fa-clock" /> 1 days left
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-12 col-md-4 col-lg-4">
-        <div className="card h-100">
-          <img
-            className="card-img-top p-2"
-            src="https://via.placeholder.com/150"
-            alt="Card image cap"
-          />
-          <div className="card-body" style={{ padding: "1rem" }}>
-            <div className="row py-2">
-              <div className="col-9">
-                <span
-                  style={{ textDecoration: "line-through", paddingRight: 5 }}
-                >
-                  $ 0
-                </span>
-                <span style={{ color: "#2a89ee" }}>Free</span>
-              </div>
-              <div className="col-3">
-                <img
-                  src={require("../../assets/img/google-logo.png")}
-                  alt
-                  sizes
-                  srcSet
-                  style={{ width: 25, height: 25 }}
-                />
-              </div>
-            </div>
-            <h5 className="card-title" style={{ fontSize: 15 }}>
-              Photoshop Mega Course – From Beginner to Super Designer
-            </h5>
-            <div className="row">
-              <div className="col-12 col-lg-6" style={{ fontSize: 12 }}>
-                <p>
-                  <span style={{ textTransform: "uppercase", fontSize: 10 }}>
-                    PHOTOSHoP
-                  </span>
-                  <br />
-                  <span style={{ fontSize: 10 }}>Udemy</span>
-                </p>
-              </div>
-              <div className="col-12 col-lg-6" style={{ fontSize: 12 }}>
-                <p className="text-muted">
-                  <i className="far fa-clock" /> 1 days left
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container py-2">
+
+      
+      <div className="container mt-4">
         <div
           className="pagination"
           style={{ display: "flex", justifyContent: "center" }}
