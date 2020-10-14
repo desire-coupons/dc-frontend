ef78f13b40ab880a4fc28a51cf0295b715a82174
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2020-10-14 10:13:29 +0530

    Formatted by Prettier

--- nav.js
+++ nav.js
@@ -6,53 +6,53 @@ const cancelBtnSearch = document.querySelector(".cancel-icon-search");
 const items = document.querySelector(".nav-items");
 const form = document.querySelector("form");
 menuBtn.onclick = () => {
-  items.classList.add("active");
-  menuBtn.classList.add("hide");
-  /* searchBtn.classList.add("hide"); */
-  cancelBtn.classList.add("show");
+    items.classList.add("active");
+    menuBtn.classList.add("hide");
+    /* searchBtn.classList.add("hide"); */
+    cancelBtn.classList.add("show");
 };
 cancelBtn.onclick = () => {
-  items.classList.remove("active");
-  menuBtn.classList.remove("hide");
-  /* searchBtn.classList.remove("hide"); */
-  /* cancelBtn.classList.remove("show"); */
+    items.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    /* searchBtn.classList.remove("hide"); */
+    /* cancelBtn.classList.remove("show"); */
 
-  form.classList.remove("active");
-  cancelBtn.style.color = "#ff3d00";
+    form.classList.remove("active");
+    cancelBtn.style.color = "#ff3d00";
 };
 cancelBtnSearch.onclick = () => {
-  form.classList.remove("active");
-  searchBtn.classList.remove("hide");
-  cancelBtnSearch.classList.remove("show");
-  cancelBtnSearch.style.color = "#171c24";
+    form.classList.remove("active");
+    searchBtn.classList.remove("hide");
+    cancelBtnSearch.classList.remove("show");
+    cancelBtnSearch.style.color = "#171c24";
 };
 searchBtn.onclick = () => {
-  form.classList.add("active");
-  searchBtn.classList.add("hide");
-  cancelBtnSearch.classList.add("show");
-  cancelBtnSearch.style.color = "#ff3d00";
+    form.classList.add("active");
+    searchBtn.classList.add("hide");
+    cancelBtnSearch.classList.add("show");
+    cancelBtnSearch.style.color = "#ff3d00";
 };
 
 /* When the user clicks on the button, 
       toggle between hiding and showing the dropdown content */
 function navbardropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (e) {
-  if (!e.target.matches(".dropbtn")) {
-    var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains("show")) {
-      myDropdown.classList.remove("show");
+    if (!e.target.matches(".dropbtn")) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains("show")) {
+            myDropdown.classList.remove("show");
+        }
     }
-  }
 };
 
 /* When the user clicks on the button, 
       toggle between hiding and showing the dropdown content */
 function categoriesdropdown() {
-  document.getElementById("categoriesDropdown").classList.toggle("show");
+    document.getElementById("categoriesDropdown").classList.toggle("show");
 }
 /* 
 // Close the dropdown if the user clicks outside of it
